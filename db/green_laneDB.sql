bd9cd67bcb4a45dec29aee25a5b4fef321826660
Muneer Lalji <muneerlalji@gmail.com>
2023-04-16 20:20:58 -0400

    Auto_increment changes to database

--- db/green_laneDB.sql
+++ db/green_laneDB.sql
@@ -7,23 +7,25 @@ CREATE SCHEMA IF NOT EXISTS `green_lane` DEFAULT CHARACTER SET latin1 ;
 
 show databases;
 
-use green_laneFinal;
+use green_lane;
 
 create table Bikes (
-    maintenanceStatus boolean not null,
-    rideStatus boolean not null,
-    type varchar(50) not null,
-    location varchar(50) not null,
-    bikeID int,
-    primary key (bikeID)
+    bikeID int not null AUTO_INCREMENT,
+    maintenanceStatus boolean,
+    rideStatus boolean,
+    type varchar(50),
+    location varchar(50),
+    DS_stationID int not null,
+    primary key (bikeID),
+    FOREIGN KEY (DS_stationID) REFERENCES DockingStation(stationID)
 
 );
 
 create table City (
-    cityID int,
-    location varchar(50) not null ,
-    numStations int not null ,
-    numBikes int not null ,
+    cityID int not null AUTO_INCREMENT,
+    location varchar(50),
+    numStations int,
+    numBikes int,
     B_bikeID int,
     primary key (cityID),
     FOREIGN KEY (B_bikeID) REFERENCES Bikes(bikeID)
@@ -32,39 +34,43 @@ create table City (
 );
 
 create table Users (
-    userID int,
+    userID int not null AUTO_INCREMENT,
     lName varchar(50) not null,
     fName varchar(50) not null,
-    dob date not null ,
+    dob date,
     address varchar(50),
     email varchar(100) unique not null,
-    primary key (userID)
+    CR_rideID int,
+    primary key (userID),
+    FOREIGN KEY (CR_rideID) references CurrentRide(rideID)
 );
 
 create table DockingStation (
-    stationID int,
-    docks int not null ,
+    stationID int not null AUTO_INCREMENT,
+    docks int,
     location varchar(50),
-    numBikes int not null ,
-    B_bikeID int not null ,
+    numBikes int,
+    B_bikeID int,
     primary key (stationID),
     FOREIGN KEY (B_bikeID) REFERENCES Bikes(bikeID)
 );
 
 create table WorkOrders (
-    reportID int,
+    reportID int not null AUTO_INCREMENT,
     location varchar(50),
-    timeResolved datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null ,
-    timeReported datetime default CURRENT_TIMESTAMP not null ,
+    timeResolved datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    timeReported datetime default CURRENT_TIMESTAMP,
     reportType varchar(50),
     B_bikeID int,
+    E_employeeID int,
     primary key (reportID),
-    foreign key (B_bikeID) references Bikes(bikeID)
+    foreign key (B_bikeID) references Bikes(bikeID),
+    foreign key (E_employeeID) references RepairCrew(employeeID)
 
 );
 
 create table RepairCrew (
-    employeeID int,
+    employeeID int not null AUTO_INCREMENT,
     location varchar(50),
     lName varchar(50),
     fName varchar(50),
@@ -74,28 +80,27 @@ create table RepairCrew (
 );
 
 create table CurrentRide (
+    rideID int not null AUTO_INCREMENT,
     B_bikeID int,
     U_userID int,
-    rideID int,
     startTime datetime default CURRENT_TIMESTAMP,
-    PRIMARY KEY (U_userID, B_bikeID),
+    PRIMARY KEY (rideID),
     FOREIGN KEY (U_userID) REFERENCES Users(userID),
     FOREIGN KEY (B_bikeID) REFERENCES Bikes(bikeID)
 
 );
 
-ALTER TABLE CurrentRide ADD INDEX idx_rideID (rideID);
-
 create table RideHistory (
+    historyID int not null AUTO_INCREMENT,
     startTime datetime default CURRENT_TIMESTAMP,
     endTime datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
     startLoc varchar(50),
     endLoc varchar(50),
     rating int CHECK (rating >= 1 AND rating <= 5),
     CR_rideID int,
-    primary key (CR_rideID),
+    primary key (historyID),
     foreign key (CR_rideID) references CurrentRide(rideID)
-);
 
 
--- failed to add the foreign key constraint missing index for constraint 'RideHistroy_ibfk_1' in the referenced table CurrentRide
+
+)
