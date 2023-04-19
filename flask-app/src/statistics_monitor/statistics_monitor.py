bbaefde79d54b9e2f72388ea590eca11e57bb5c4
Muneer Lalji <muneerlalji@gmail.com>
2023-04-19 10:05:08 -0400

    Users and statistics_monitor routes cont.

--- flask-app/src/statistics_monitor/statistics_monitor.py
+++ flask-app/src/statistics_monitor/statistics_monitor.py
@@ -18,3 +18,64 @@ def get_all_users():
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+#Get all docking stations
+@statistics_monitor.route('/stations', methods=['GET'])
+def get_all_stations():
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from DockingStation')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+#Add new station
+@statistics_monitor.route('/stations', methods=['POST'])
+def add_station():
+    current_app.logger.info('processing form data')
+    req_data = request.get_json()
+    current_app.logger.info(req_data)
+    
+    docks = req_data['docks']
+    location = req_data['location']
+    num_bikes = 0
+    city = req_data['city']
+
+    insert_stmt = 'INSERT into DockingStation (docks, location, numBikes, C_cityID) VALUES ('
+    insert_stmt += docks + ', ' + location + ", " + num_bikes + ", " + city + ')'
+
+    current_app.logger.info(insert_stmt)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(insert_stmt)
+    db.get_db().commit()
+    return 'Success'
+
+#Add new bike
+@statistics_monitor.route('/bikes', methods=['POST'])
+def add_bike():
+    current_app.logger.info('processing form data')
+    req_data = request.get_json()
+    current_app.logger.info(req_data)
+    
+    maintenance_status = 'false'
+    ride_status = 'false'
+    type = req_data['type']
+    location = req_data['location']
+    station = req_data['station']
+    city = req_data['city']
+
+    insert_stmt = 'INSERT into Bikes (maintenenceStatus, rideStatus, type, location, DS_stationID, C_cityID) VALUES ('
+    insert_stmt += maintenance_status + ', ' + ride_status + ", " + type + ", " + location + ', ' + station + ', ' + city + ')'
+
+    current_app.logger.info(insert_stmt)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(insert_stmt)
+    db.get_db().commit()
+    return 'Success'
--- flask-app/src/users/users.py
+++ flask-app/src/users/users.py
@@ -8,7 +8,7 @@ users = Blueprint('users', __name__)
 @users.route('/users/<email>', methods=['GET'])
 def get_user(email):
     cursor = db.get_db().cursor()
-    cursor.execute('select * from customers where email = {0}'.format(email)) #TODO fix query
+    cursor.execute(f'select * from Users where email = {email}')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -32,7 +32,7 @@ def add_user():
     u_dob = req_data['dob']
     u_address = req_data['address']
 
-    insert_stmt = 'INSERT into users (first_name, last_name, email, dob, address) VALUES ('
+    insert_stmt = 'INSERT into Users (first_name, last_name, email, dob, address) VALUES ('
     insert_stmt += u_first + ', ' + u_last + ", " + u_email + ", " + u_dob + ", " + u_address + ')'
 
     current_app.logger.info(insert_stmt)
@@ -42,4 +42,51 @@ def add_user():
     db.get_db().commit()
     return 'Success'
 
-#
+#Change bike usage status
+@users.route('/bikes/<bikeID>', methods=['PUT'])
+def change_bike_status(bikeID):
+    current_app.logger.info('processing form data')
+    req_data = request.get_json()
+    current_app.logger.info(req_data)
+    
+    new_status = req_data['status']
+
+    update_stmt = 'UPDATE Bikes SET rideStatus '
+    update_stmt += new_status + ' WHERE bikeID = ' + bikeID + ';'
+
+    current_app.logger.info(update_stmt)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(update_stmt)
+    db.get_db().commit()
+    return 'Success'
+
+#Get all bikes in a particular city
+@users.route('/bikes/<city>', methods=['GET'])
+def get_bike_city(city):
+    cursor = db.get_db().cursor()
+    cursor.execute(f'select * from Bikes where C_cityID = {city}')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+#Get all stations in a particular city
+@users.route('/city/<city>', methods=['GET'])
+def get_station_city(city):
+    cursor = db.get_db().cursor()
+    cursor.execute(f'select * from DockingStation where C_cityID = {city}')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
